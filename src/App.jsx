923e0f67fe0c308a44c81602b83d5e853e0f0e76
KiptooMannu <Kkgg7241@gmail.com>
2024-06-30 14:13:12 +0300

    changes made

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,34 @@ import "./App.css"
 const App = () => {
   return (
     <div>
-      <section className="h-screen bg-green-600 m-12 flex items-center justify-center text-center text-3xl">
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-          Nesciunt ad quo earum ut perspiciatis, odio aperiam in unde sit, 
-          officiis dolorem, facere aut minima placeat obcaecati. 
-          Commodi voluptatem hic quis?
-        </p>
-      </section>
+
+ <div className="card">
+  <p>This is a card</p>
+ </div>
+
+ <div className="card">
+  <p>This is a card</p>
+ </div>
+
+ <div className="card">
+  <p>This is a card</p>
+ </div>
+
+ <div className="card">
+  <p>This is a card</p>
+ </div>
+
+ <div className="card">
+  <p>This is a card</p>
+ </div>
+
+ <div className="card">
+  <p>This is a card</p>
+ </div>
+
+ <div className="card">
+  <p>This is a card</p>
+ </div>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@ export default {
   content: ["./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "twitter-blue":"#1DA1F2",
+       },
+    },
   },
   plugins: [],
 }
