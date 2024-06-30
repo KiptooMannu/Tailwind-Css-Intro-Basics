e830c40c9f3a3967cfa7d5c97adfec64795542e1
KiptooMannu <Kkgg7241@gmail.com>
2024-06-30 12:25:30 +0300

    tailwind

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css"
+
+const App = () => {
+  return (
+    <div>
+      <section className="h-screen bg-green-600 m-12 flex items-center justify-center text-center text-3xl">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+          Nesciunt ad quo earum ut perspiciatis, odio aperiam in unde sit, 
+          officiis dolorem, facere aut minima placeat obcaecati. 
+          Commodi voluptatem hic quis?
+        </p>
+      </section>
+    </div>
+  )
+}
+
+export default App
